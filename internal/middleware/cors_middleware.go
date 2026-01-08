14d5d4946285b36ebec74273295faa956695a4a2
mohamadarif03 <mohamadarif5392@gmail.com>
2026-01-08 10:35:33 +0700

    izin akses 3000

--- internal/middleware/cors_middleware.go
+++ internal/middleware/cors_middleware.go
@@ -4,7 +4,15 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := c.Request.Header.Get("Origin")
+		allowedOrigins := []string{"http://localhost:5173", "http://localhost:3000"}
+
+		for _, o := range allowedOrigins {
+			if o == origin {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
